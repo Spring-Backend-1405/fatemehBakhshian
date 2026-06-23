7a6e206a527494809bd48ccd1a1393275ed0d4cc
Fatemeh Bakhshian <bakhshainf@gmail.com>
2026-06-23 18:41:23 +0330

    Add base middleware: cors, morgan, helmet , rateLimit

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 import express from 'express';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import globalErrorHandler from './app/middleware/globalErrorHandler/index';
+import AppError from './app/utils/appError';
 
 const app = express();
+app.use(helmet());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  }),
+);
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 
 
-app.use(globalErroreHandler);
+// project routes
 
+
+
+
+
+app.all(/.*/, (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 export default app;
---
+++ server.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+
+dotenv.config('./.env');
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`server is running, swagger local address is: http://localhost:${port}/api-docs `);
+});
